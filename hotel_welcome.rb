aaf34e7ba7ad2ee8bf658e1b82313dcb9146d8e9
kileysel <kiley@selmanhouse.com>
2018-06-06 16:49:44 +0000

    Done.

---
+++ hotel_welcome.rb
@@ -0,0 +1,4 @@
+def tv_welcome (room_number, name)
+  puts "hello #{name}, welcome to #{room_number}"
+end
+tv_welcome("312", "kiley")
---
+++ loops.rb
@@ -0,0 +1,13 @@
+# 10.times do
+#   puts "where is my lunch!"
+# end
+# loop do 
+#   puts "grumble grumble"
+#   puts "I am so hungry!!"
+#   puts ":("
+# end
+def dance moves_slay
+  
+end
+end
+dance
