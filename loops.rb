8cf93b5f4f48c55f7e310db7b1180564fbe331a9
kileysel <kiley@selmanhouse.com>
2018-06-06 17:33:41 +0000

    Done.

--- loops.rb
+++ loops.rb
@@ -6,8 +6,19 @@
 #   puts "I am so hungry!!"
 #   puts ":("
 # end
-def dance moves_slay
-  
-end
-end
-dance
+#DEFINE KODE WITH KLOSSY METHOD 
+def kwk_camp
+  days = 2 
+  days_left = 7
+#MAKE A WHILE loop
+  while days < 10 
+  puts "Keep coming to camp!"
+  #days = days +1 
+  puts "you have #{days_left} days left"
+  days += 1
+  days_left -= 1 
+  end
+  puts "camp is over :("
+end 
+kwk_camp
+
